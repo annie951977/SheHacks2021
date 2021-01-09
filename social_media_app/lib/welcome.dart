69fb3ea78b8a433de03a805f7b56e0ab4dc7bda1
Annie <liuzr.annie@gmail.com>
2021-01-09 13:10:14 -0500

    Found some source code
    https://github.com/harshit2106/Flutter-Firebase-sign-in

--- social_media_app/lib/welcome.dart
+++ social_media_app/lib/welcome.dart
@@ -1,10 +1,8 @@
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_auths/controllers/authentications.dart';
-import 'package:flutter_auths/pages/signupScreen.dart';
-import 'package:flutter_auths/pages/tasks.dart';
-import 'package:form_field_validator/form_field_validator.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:authentification/Login.dart';
+import 'SignUp.dart';
 
 class Start extends StatefulWidget {
   @override
@@ -12,28 +10,116 @@ class Start extends StatefulWidget {
 }
 
 class _StartState extends State<Start> {
+
+  navigateToLogin()async{
+
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> Login()));
+  }
+
+   navigateToRegister()async{
+
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUp()));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Container(
-         child: Column(
-        children: <Widget>[
+        child: Column(
+          children: <Widget>[
 
-        Container(
+            SizedBox(height: 35.0),
 
-          child: Image(image: AssetImage(assetName),),
-          ), //add logo when we get it
-          RichText(
-            text: TextSpan(
-              text: "Welcome to ", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white)
-            )
-          )
-      ]
-      )
+            Container(
+              height: 400,
+
+              child: Image(image: AssetImage("images/12-120520_captain-marvel-clipart-clip-art-captain-marvel-clipart.png"),
+              fit: BoxFit.contain,
+              ),
+            ),
+
+            SizedBox(height : 20),
+
+            RichText(
+              
+              text: TextSpan(
+                text: 'Welcome to ', style: TextStyle(
+                 fontSize: 25.0,
+                 fontWeight: FontWeight.bold,
+                 color: Colors.black
+                ),
+
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'X Groceries', style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.orange)
+                  )
+                ]
+              ) 
+            ),
+            SizedBox(height: 10.0),
+
+            Text('Fresh Groceries Delivered at your Doorstep',style: TextStyle(color:Colors.black),),
+
+            SizedBox(height: 30.0),
+
+
+            Row( mainAxisAlignment: MainAxisAlignment.center,
+
+              children: <Widget>[
+
+                RaisedButton(
+                  padding: EdgeInsets.only(left:30,right:30),
+                  
+                  onPressed: navigateToLogin,
+                  child: Text('LOGIN',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  ),),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Colors.orange
+                ),
+
+                SizedBox(width:20.0),
+
+                RaisedButton(
+                  padding: EdgeInsets.only(left:30,right:30),
+                  
+                  onPressed: navigateToRegister,
+                  child: Text('REGISTER',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  ),),
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Colors.orange
+                ),
+
+              ],
+            ),
+
+            
+                SizedBox(height : 20.0),
+
+                SignInButton(
+                Buttons.Google,
+                text: "Sign up with Google",
+                onPressed: () {},
+             )
+          ],
+        ),
+      ),
       
-      )
-     
     );
   }
-  // tutorial credit: https://www.youtube.com/channel/UCc8DBMjBouqHmxQYP1B90QA
+  // credit: 
 }
